5ad2cf8fef7c80f2283d799bb85d2b6077b2c0ff
Venícius Dobler <veniciusdobler840@gmail.com>
2024-03-06 17:39:34 -0300

    fix: corrigido o bug de clicar em entrar sem colocar nada e dar erro 404 na tela de login

--- login/views.py
+++ login/views.py
@@ -2,12 +2,11 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import AuthenticationForm
 
 from .forms import UserForm
 
 # Create your views here.
-
-
 def registro(request):
     template_name = 'registro.html'
     context = {}
@@ -23,21 +22,24 @@ def registro(request):
     context['form'] = form
     return render(request, template_name, context)
 
-
 def login_view(request):
     template_name = 'login.html'
     if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('home')
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                print('Usuário ou senha incorretos.')
         else:
-            print('Usuário ou senha incorretos.')
-    return render(request, template_name, {})
-
-
+            print('Formulário inválido.')
+    else:
+        form = AuthenticationForm()
+    return render(request, template_name, {'form': form})
 
 @login_required(login_url='/login/')
 def user_logout(request):
