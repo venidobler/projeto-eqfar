46275eef9d78da8bb3852f3277cf69a6b589d548
Venícius Dobler <veniciusdobler840@gmail.com>
2024-04-01 17:55:05 -0300

    feat: grafico gantt adicionado à tela index.html

--- gantt/models.py
+++ gantt/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from equipments.models import Equipamento
 
 class Gantt(models.Model):
     name = models.CharField(max_length=200) 
@@ -7,6 +8,7 @@ class Gantt(models.Model):
     responsible = models.ForeignKey(User, on_delete=models.CASCADE)
     week_number = models.CharField(max_length=2, blank=True)
     finish_date = models.DateField()
+    equipment = models.ForeignKey(Equipamento, on_delete=models.CASCADE, null=True, default=None)  # Adicionando o campo de equipamento
 
 #string representation method
     def __str__(self):
@@ -17,3 +19,5 @@ class Gantt(models.Model):
         if self.week_number == "":
             self.week_number = self.start_date.isocalendar()[1]
         super().save(*args, **kwargs)
+        
+        
--- gantt/views.py
+++ gantt/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
 from gantt.models import Gantt
+from equipments.models import Equipamento  # Importe o modelo Equipamento aqui
 import pandas as pd
 from plotly.offline import plot
 import plotly.express as px
+
 def index(request):
-    qs = Gantt.objects.all()
+    equipments = Equipamento.objects.all()  # Obtém todos os equipamentos para a opção de seleção
+    selected_equipment_id = request.GET.get('equipment')  # Obtém o ID do equipamento selecionado (se houver)
+    
+    if selected_equipment_id:
+        # Filtra as tarefas pelo equipamento selecionado
+        qs = Gantt.objects.filter(equipment_id=selected_equipment_id)
+    else:
+        # Caso nenhum equipamento seja selecionado, retorna todas as tarefas
+        qs = Gantt.objects.all()
+
     projects_data = [
         {
             'Project': x.name,
@@ -19,5 +30,5 @@ def index(request):
     )
     fig.update_yaxes(autorange="reversed")
     gantt_plot = plot(fig, output_type="div")
-    context = {'plot_div': gantt_plot}
+    context = {'plot_div': gantt_plot, 'equipments': equipments, 'selected_equipment_id': selected_equipment_id}
     return render(request, 'pages/index.html', context)
