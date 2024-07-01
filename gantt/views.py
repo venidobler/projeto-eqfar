dd8b8d31b17ef59ce4709d6cdab358dbc7cadd08
Venícius Rocha Dobler <veniciusdobler840@gmail.com>
2024-07-01 10:13:34 -0300

    feat: exportar relatório adicionado

--- gantt/views.py
+++ gantt/views.py
@@ -1,19 +1,19 @@
 from django.shortcuts import render
 from gantt.models import Gantt
-from equipments.models import Equipamento  # Importe o modelo Equipamento aqui
+from equipments.models import Equipamento
 import pandas as pd
 from plotly.offline import plot
 import plotly.express as px
+from django.http import HttpResponse
+import csv
 
 def index(request):
-    equipments = Equipamento.objects.all()  # Obtém todos os equipamentos para a opção de seleção
-    selected_equipment_id = request.GET.get('equipment')  # Obtém o ID do equipamento selecionado (se houver)
+    equipments = Equipamento.objects.all()
+    selected_equipment_id = request.GET.get('equipment')
     
     if selected_equipment_id:
-        # Filtra as tarefas pelo equipamento selecionado
         qs = Gantt.objects.filter(equipment_id=selected_equipment_id)
     else:
-        # Caso nenhum equipamento seja selecionado, retorna todas as tarefas
         qs = Gantt.objects.all()
 
     projects_data = [
@@ -32,3 +32,26 @@ def index(request):
     gantt_plot = plot(fig, output_type="div")
     context = {'plot_div': gantt_plot, 'equipments': equipments, 'selected_equipment_id': selected_equipment_id}
     return render(request, 'pages/index.html', context)
+
+def exportar_relatorio(request):
+    start_date = request.GET.get('start_date')
+    end_date = request.GET.get('end_date')
+
+    if start_date and end_date:
+        qs = Gantt.objects.filter(start_date__gte=start_date, finish_date__lte=end_date)
+    else:
+        qs = Gantt.objects.all()
+
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="relatorio.csv"'
+
+    writer = csv.writer(response)
+    writer.writerow(['Projeto', 'Data de Início', 'Data de Término', 'Responsável'])
+
+    for task in qs:
+        writer.writerow([task.name, task.start_date, task.finish_date, task.responsible.username])
+
+    return response
+
+def relatorio(request):
+    return render(request, 'pages/relatorio.html')
