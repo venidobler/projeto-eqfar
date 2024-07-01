3d4c362096c393c91871affb3c4abf7a14aab278
Venícius Rocha Dobler <veniciusdobler840@gmail.com>
2024-07-01 14:02:47 -0300

    fix: empréstimo removido

--- loans/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('emprestimo/', views.emprestimo_view, name='emprestimo_view'),
-    path('devolucao/', views.devolucao_view, name='devolucao_view'),
-]
